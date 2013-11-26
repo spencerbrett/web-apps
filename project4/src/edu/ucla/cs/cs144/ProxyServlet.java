73bb9dc0246f2022bd243ae3547fae98be49109b
Daniel <warbird_dkd@yahoo.com>
2013-11-25 21:00:08 -0800

    Proxy servlet code. Mostly working. Not complete

--- project4/src/edu/ucla/cs/cs144/ProxyServlet.java
+++ project4/src/edu/ucla/cs/cs144/ProxyServlet.java
@@ -1,7 +1,16 @@
 package edu.ucla.cs.cs144;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,11 +18,36 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class ProxyServlet extends HttpServlet implements Servlet {
-       
-    public ProxyServlet() {}
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
-        // your codes here
+    public ProxyServlet() {
+    }
+
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+        
+        PrintWriter out = response.getWriter();
+        
+        String query = request.getParameter("query");
+        String charset = "UTF-8";
+
+        URL url = new URL("http://google.com/complete/search?output=toolbar&q="
+                + URLEncoder.encode(query, charset));
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Accept-Charset", charset);
+        conn.setRequestProperty("Content-type", "text/xml");
+
+        StringBuffer content = new StringBuffer();
+        BufferedReader googleResponse = new BufferedReader(
+                new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = googleResponse.readLine()) != null) {
+          content.append(line + "\n");
+        }
+        
+        conn.disconnect();
+        
+        response.setContentType("text/xml;charset=UTF-8");
+        out.write(content.toString());
     }
 }
